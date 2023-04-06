9e75bdb4dfa505dbb1c0eef556837a944325176c
ben lim <lim6112j@gmail.com>
2023-04-06 11:02:21 +0900

    minimize for pi4

--- lua/benlim/plugins/lsp/lspconfig.lua
+++ lua/benlim/plugins/lsp/lspconfig.lua
@@ -150,17 +150,17 @@ lspconfig["elmls"].setup({
 	on_attach = on_attach,
 })
 -- rust tools
-local rt = require("rust-tools")
-rt.setup({
-	server = {
-		on_attach = function(_, bufnr)
-			-- hover action
-			vim.keymap.set("n", "C-space", rt.hover_actions.hover_actions, { buffer = bufnr })
-			-- code action groups
-			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-		end,
-	},
-})
+--local rt = require("rust-tools")
+--rt.setup({
+--	server = {
+--		on_attach = function(_, bufnr)
+--			-- hover action
+--			vim.keymap.set("n", "C-space", rt.hover_actions.hover_actions, { buffer = bufnr })
+--			-- code action groups
+--			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+--		end,
+--	},
+--})
 
 -- LSP Diagnostics Options Setup
 local sign = function(opts)
